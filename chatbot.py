69270adecd4d8596913e478db2d83168d4dec74b
Vuyanzi <vuyanziglorious@gmail.com>
2019-12-13 17:06:32 +0300

    used exact matches for responses

--- chatbot.py
+++ chatbot.py
@@ -9,17 +9,17 @@ from sklearn.svm import SVC # Support Vector Classification model
 output_format = "IN: {input}\nOUT: {output}\n" + "_"*50
 # hard-coded exact questions
 responses_exact = {
-    "what would you like to eat tonight?": "Pasta with salmon and red pesto please!",
-    "what time will you be home tonight?": "I will be home around 6 pm.",
-    "default": "I love you too!"
+    "How was your day today? Any up and low moments?": "I just found out that I lost my job",
+    "How are you feeling right now?": "Am pretty sad to be honest",
+    "default": "It only gets better with time"
 }
 
 def respond_exact(text):
-    response = responses_exact.get(text.lower(), responses_exact['default'])
+    response = responses_exact.get(text, responses_exact['default'])
     return(output_format.format(input=text, output=response))
     
 
-print(respond_exact("What would you like to eat tonight?"))
+print(respond_exact("How was your day today? Any up and low moments?"))
 print("_"*50)
 print(respond_exact("What time will you be home tonight?"))
 print("_"*50)
